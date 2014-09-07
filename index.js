de441b03dd55a0b69d47d990e8a52f8f0687f4bb
Randall Degges <r@rdegges.com>
2014-09-06 17:10:22 -0700

    Writing Google Analytics middleware.

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var jade = require('jade');
+
+/**
+ * Make a *googleAnalyticsScript* variable available in all templates rendered.
+ *
+ * @param {String} this site's Google Analytics Property Id
+ * @return {Function} middleware
+ * @api public
+ */
+module.exports = function(propertyId) {
+  var googleAnalyticsScript = jade.renderFile('./analytics.jade', {
+    googleAnalyticsPropertyId: propertyId
+  });
+
+  return function(req, res, next) {
+    res.locals.googleAnalyticsScript = googleAnalyticsScript;
+    next();
+  };
+};
